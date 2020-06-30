1894ed47244823c7e786bddb3e927a6801471eb3
Adriano Kerber <kerberpro@gmail.com>
2020-06-30 16:48:50 -0300

    Add SpecFlow project with Calculator test example

---
+++ Crescer.Spotify.Specs/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crescer.Spotify.Specs
+{
+    class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
---
+++ Crescer.Spotify.Specs/CalculatorSteps.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace Crescer.Spotify.Specs
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private Calculator calculator = new Calculator();
+        private int result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
