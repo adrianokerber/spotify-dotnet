a3d411fe67d164a6899bbcbc5b26f383a6d548d5
Adriano Kerber <kerberpro@gmail.com>
2020-07-01 17:55:50 -0300

    Create MusicasController test with feature

---
+++ Crescer.Spotify.Specs/Controllers/MusicasController/Features/FindMusicById.feature
@@ -0,0 +1,10 @@
+﻿Feature: FindMusicById
+	In order to avoid silly mistakes
+	As a worried user
+	I want to retrieve a music with the same inputed id
+
+@MusicasController
+Scenario: Retrieve a music by ID
+	Given I have the id "5eec28eb6e50a13a88dfffb6"
+	When I call GET music
+	Then the result should be a music with the same "5eec28eb6e50a13a88dfffb6" id
---
+++ Crescer.Spotify.Specs/Controllers/MusicasController/Steps/GetMusicaByIdSuccessSteps.cs
@@ -0,0 +1,47 @@
+﻿using Crescer.Spotify.Dominio.Contratos;
+using Crescer.Spotify.Dominio.Servicos;
+using Crescer.Spotify.WebApi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Bson;
+using Moq;
+using TechTalk.SpecFlow;
+
+namespace Crescer.Spotify.Specs.Steps
+{
+    [Binding]
+    public class GetMusicaByIdSuccessSteps
+    {
+        private MusicasController controller;
+        private string givenId;
+        private string resultId;
+
+        [Before]
+        public void Before()
+        {
+            Mock<IMusicaRepository> mockRepoMusicaRepository = new Mock<IMusicaRepository>();
+            Mock<MusicaService> mockRepoMusicaService = new Mock<MusicaService>(mockRepoMusicaRepository.Object);
+
+            controller = new MusicasController(mockRepoMusicaRepository.Object, mockRepoMusicaService.Object);
+        }
+
+        [Given(@"I have the id ""(.*)""")]
+        public void GivenIHaveTheId(string id)
+        {
+            givenId = id;
+        }
+        
+        [When(@"I call GET music")]
+        public void WhenICallGETMusic()
+        {
+            var result = controller.Get(givenId);
+
+            resultId = result.ToBsonDocument().GetValue("id").ToString();
+        }
+
+        [Then(@"the result should be a music with the same ""(.*)"" id")]
+        public void ThenTheResultShouldBeAMusicWithTheSameId(string expectedId)
+        {
+            Assert.AreSame(expectedId, resultId);
+        }
+    }
+}
